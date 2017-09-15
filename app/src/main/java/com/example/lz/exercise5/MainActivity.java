e3ec777a9b69d982b4f52101a0f2b185c4f3c4e3
xiangcundou <1916583212@qq.com>
2017-09-15 10:18:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lz/exercise5/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lz.exercise5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button_tel);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18195040398"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
